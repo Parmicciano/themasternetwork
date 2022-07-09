4a36abc9f60cd41f2d897b46b5a65a24fa5a9b57
Parmicciano <85128225+Parmicciano@users.noreply.github.com>
2022-07-09 22:51:05 +0200

    AA

--- dist/_nuxt/static/1656627116/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Freleases","\u002F","\u002Fimprove","\u002Ftechnical-document","\u002Fcreate-a-vm","\u002FFacturation","\u002Finstallation-virtualize","\u002Finstallation","\u002Fhost-a-website"]})
